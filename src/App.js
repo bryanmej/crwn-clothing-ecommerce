397120a046f2a6506770c201aff273732e5577eb
bryanmej <bryanmej@gmail.com>
2019-10-06 01:24:26 -0500

    seperated by components and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./hompageComponent";
+import HomePage from "./pages/homepage/hompageComponent";
 
 function App() {
   return (
