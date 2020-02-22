6e6508c984c01ec5e4d6033d68483b0a4a5fa51c
bryanmej <bryanmej@gmail.com>
2020-02-22 16:22:49 -0600

    removed redux logger from production build and deployed to heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 import logger from "redux-logger";
 
-const middleware = [logger];
+const middleware = [];
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 
